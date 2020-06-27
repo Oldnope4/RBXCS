afc92e76aebdbaf8fe8dd43c78e2dd7856b8b3cc
Oldnope4 <66508812+Oldnope4@users.noreply.github.com>
2020-06-26 22:43:31 -0400

    Create the Gui Builder

---
+++ Menu Cores/Evolution UI/GuiBuilder.lua
@@ -0,0 +1,7 @@
+-- Created by p0s_0 on Roblox and maintained by himself.
+-- GuiBuilder.lua
+-- 6/26/2020 10:42 PM EDT
+
+-- Variables --
+local CoreGui = game:GetService("CoreGui")
+-- will just copy into RBX Studio then continue from there lol
